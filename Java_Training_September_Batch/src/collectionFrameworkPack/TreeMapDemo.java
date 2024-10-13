e0503d3337b0541d44e45a0a533f5cacb83dabdb
Admin <prashantsoni2810@gmail.com>
2024-10-13 13:58:26 +0530

    13 october code

---
+++ Java_Training_September_Batch/src/collectionFrameworkPack/TreeMapDemo.java
@@ -0,0 +1,29 @@
+package collectionFrameworkPack;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+
+	public static void main(String[] args) {
+
+		// map will store data in the form key and value pair
+		// search operation is faster in map
+
+		Map<String, String> map = new TreeMap<String, String>();
+
+		map.put("19", "India");
+		map.put("32", "Indonesia");
+		map.put("13", "Australia");
+		map.put("42", "Canada");
+		map.put("15", "Bermuda");
+
+		for (Map.Entry<String, String> entryset : map.entrySet()) {
+			System.out.println(entryset.getKey() + "  " + entryset.getValue());
+		}
+
+		// HashMap //LinkHashMap // TreeMap
+	}
+
+}
